e997f0aebb5d695c7d379c5749e59ef542c30e86
Ross MacDonald <rmacdonald@dal.ca>
2020-01-05 09:25:37 -0500

    Updates

---
+++ precache-manifest.57c281ff528cb95e10e6a95fbd7b9bbb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d3d27a96029093bca2b33edcb11f4c8",
+    "url": "/Pomodoro-Tracker/index.html"
+  },
+  {
+    "revision": "4376611b022ef1abb87c",
+    "url": "/Pomodoro-Tracker/static/css/2.b90ce945.chunk.css"
+  },
+  {
+    "revision": "a273b09541f51446faa4",
+    "url": "/Pomodoro-Tracker/static/css/main.4081c0b0.chunk.css"
+  },
+  {
+    "revision": "4376611b022ef1abb87c",
+    "url": "/Pomodoro-Tracker/static/js/2.985f219d.chunk.js"
+  },
+  {
+    "revision": "a273b09541f51446faa4",
+    "url": "/Pomodoro-Tracker/static/js/main.e9176d81.chunk.js"
+  },
+  {
+    "revision": "2b8071e986d8fb586ee7",
+    "url": "/Pomodoro-Tracker/static/js/runtime-main.5f867df2.js"
+  }
+]);
